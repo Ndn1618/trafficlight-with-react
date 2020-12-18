8062c93fa4845a1eb8bbbefaa399af13b37f3177
Ndn1618 <ndilbar1618@gmail.com>
2020-12-18 23:10:49 +0500

    Delete unnecessary property of data object

--- src/Data.jsx
+++ src/Data.jsx
@@ -2,17 +2,14 @@ const data = [
   {
     id: 0,
     light: 'red',
-    isOn: false,
   },
   {
     id: 1,
     light: 'yellow',
-    isOn: false,
   },
   {
     id: 2,
     light: 'rgb(46, 202, 7)',
-    isOn: false,
   }
 ]
 
