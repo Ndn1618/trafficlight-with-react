9ee57139acc7897f78f3fe6f04a899f505fae125
Ndn1618 <ndilbar1618@gmail.com>
2020-12-18 10:54:01 +0500

    Add trafficlight styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 function App() {
   return (
     <>
+      <div className='container'>
+        <div className='trafficlight-box'>
+          <p className='trafficlight trafficlight__red'></p>
+          <p className='trafficlight trafficlight__yellow'></p>
+          <p className='trafficlight trafficlight__green'></p>
+        </div>
+      </div>
     </>
   );
 }
