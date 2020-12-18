3ee856e49f23694581f41f81c52ee9d20cae1137
Ndn1618 <ndilbar1618@gmail.com>
2020-12-18 13:33:27 +0500

    Make traffic light colors changeable

---
+++ src/Data.jsx
@@ -0,0 +1,19 @@
+const data = [
+  {
+    id: 0,
+    light: 'red',
+    isOn: false,
+  },
+  {
+    id: 1,
+    light: 'yellow',
+    isOn: false,
+  },
+  {
+    id: 2,
+    light: 'rgb(46, 202, 7)',
+    isOn: false,
+  }
+]
+
+export default data
